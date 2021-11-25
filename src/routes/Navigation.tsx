3d52aeba07121f22d6c2e697e927a64a0a49f011
Adrián Antón <adrtler@gmail.com>
2021-11-25 12:54:27 +0100

    Componente product card

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
-
 import logo from '../logo.svg';
+import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
+
 export const Navigation = () => {
   return (
     <BrowserRouter>
@@ -11,10 +12,10 @@ export const Navigation = () => {
           <ul>
             <li>
               <NavLink
-                to="home"
+                to="/"
                 className={({ isActive }) => (isActive ? 'nav-active' : '')}
               >
-                Home
+                Shopping
               </NavLink>
             </li>
             <li>
@@ -38,8 +39,8 @@ export const Navigation = () => {
         <Routes>
           <Route path="about" element={<h1>About Page</h1>} />
           <Route path="users" element={<h1>Users Page</h1>} />
-          <Route path="home" element={<h1>Home Page</h1>} />
-          <Route path="/*" element={<Navigate to="/home" replace />} />
+          <Route path="/" element={<ShoppingPage />} />
+          <Route path="/*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
     </BrowserRouter>
